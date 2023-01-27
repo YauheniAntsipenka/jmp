8cf23bee05d3db721e08d2bc7a7f4f63dbcd3620
Yauheni Antsipenka <Yauheni_Antsipenka@epam.com>
2023-01-28 00:47:33 +0300

    Add 24 task

--- jmp-m2-java/jmp-cloud-bank-impl/src/main/java/module-info.java
+++ jmp-m2-java/jmp-cloud-bank-impl/src/main/java/module-info.java
@@ -1,5 +1,9 @@
+import jmp.bank.api.Bank;
+import jmp.cloud.bank.impl.BankImpl;
+
 module jmp.cloud.bank.impl {
     requires transitive jmp.bank.api;
     requires jmp.dto;
     exports jmp.cloud.bank.impl;
+    provides Bank with BankImpl;
 }
--- jmp-m2-java/jmp-cloud-service-impl/src/main/java/module-info.java
+++ jmp-m2-java/jmp-cloud-service-impl/src/main/java/module-info.java
@@ -1,5 +1,9 @@
+import jmp.cloud.service.impl.ServiceImpl;
+import jmp.service.api.Service;
+
 module jmp.cloud.service.impl {
     requires transitive jmp.service.api;
     requires jmp.dto;
     exports jmp.cloud.service.impl;
+    provides Service with ServiceImpl;
 }
