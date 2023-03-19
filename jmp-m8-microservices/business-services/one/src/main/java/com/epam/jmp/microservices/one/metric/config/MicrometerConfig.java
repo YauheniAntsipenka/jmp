cbe183b5426dd57a8e7c1a332c516aa583fa7af4
Yauheni Antsipenka <Yauheni_Antsipenka@epam.com>
2023-03-19 17:15:19 +0300

    Add tasks 13-14. Implement `one` and `two` modules

---
+++ jmp-m8-microservices/business-services/one/src/main/java/com/epam/jmp/microservices/one/metric/config/MicrometerConfig.java
@@ -0,0 +1,23 @@
+package com.epam.jmp.microservices.one.metric.config;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * MicrometerConfig
+ * Date: 03/19/2023
+ *
+ * @author Yauheni Antsipenka
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class MicrometerConfig {
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
