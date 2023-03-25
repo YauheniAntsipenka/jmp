7dc4268fd031303913b985a4a3024f727637d543
Yauheni Antsipenka <Yauheni_Antsipenka@epam.com>
2023-03-25 21:38:22 +0300

    Add Apache Kafka module task

---
+++ jmp-m9-kafka/courier-app/src/main/java/com/epam/jmp/kafka/courier/config/KafkaAdminTopic.java
@@ -0,0 +1,42 @@
+package com.epam.jmp.kafka.courier.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * KafkaAdminTopic
+ * Date: 03/24/2023
+ *
+ * @author Yauheni Antsipenka
+ */
+@Configuration
+public class KafkaAdminTopic {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+    @Value(value = "${notification.topic.name}")
+    private String notificationTopicName;
+
+    @Bean
+    public KafkaAdmin admin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic notificationTopic() {
+        return TopicBuilder.name(notificationTopicName)
+            .partitions(3)
+            .replicas(1)
+            .build();
+    }
+}
